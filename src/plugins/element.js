5c4778574d9613ef78e6b963249a7f069675d752
Tao <491675919@qq.com>
2022-02-07 22:38:49 +0800

    1/8

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
+import {Form, FormItem} from 'element-ui'
+import {Input} from 'element-ui'
+// 导入弹框提示组件
+import {Message} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.prototype.$message = Message // 对Message进行全局挂载，这样所有组件都可以通过this访问$message,从而可以进行弹框提示
